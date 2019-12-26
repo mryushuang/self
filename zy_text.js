3256e0a2c8e1de123871a4c8e2b838ddaabda36f
yushuang <34976884+mryushuang@users.noreply.github.com>
2019-12-26 11:17:57 +0800

    Update zy_text.js

--- zy_text.js
+++ zy_text.js
@@ -1,11 +1,13 @@
-			function shuai(){
+						function shuai(){
+				do{
 				var x=prompt("照片上的那个同学帅吗？","")
 				var zy1="虽然已经不止一个人这么说了";
 				var zy2="但我还是很欣赏你的眼光,真诚而犀利。";
 				var zy3="生活中你一定是一个温暖、精致、正直且智慧的人，如果能成为你的朋友是我的荣幸！"+"</br>"+"——————————————谢谢！";
 				
-				if(x=="帅"){
-					alert("英雄所见略同")
+				
+					if(x=="帅"){
+					alert("谢谢")
 					document.getElementById("text1").innerHTML=zy1;
 					document.getElementById("text2").innerHTML=zy2;
 					document.getElementById("text3").innerHTML=zy3;
@@ -22,5 +24,7 @@
 //					document.getElementsByClassName("bottom_text").innerHTML="zy4";
 				}else{
 					alert("请输入标准答案")
+				     }
 				}
-			     }
+				while(x!="帅")
+			    }
